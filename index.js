44c49e56e64262091721e0f89d605822c287647c
BaurMus <musilimov_b@mail.ru>
2023-05-06 11:56:00 +0600

    connect to MongoDb/create views

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const exphbs = require("express-handlebars");
 
 const PORT = process.env.PORT || 3000;
 
-const app = express();
-
-app.listen('PORT', () => {
-  console.log('Server has been started...');
+const app = express();  
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
 });
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+async function start() {
+  try {
+    await mongoose.connect('mongodb+srv://musilimovb:bake_Js1984@cluster1.ulim0rl.mongodb.net/todos', {
+      useNewUrlParser: true,
+      useFindAndModify: false
+    });
+
+    app.listen(PORT, () => {
+      console.log('Server has been started...');
+    });
+    
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
